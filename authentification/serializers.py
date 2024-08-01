a67204ff268929324207d196fdad31278c5e88da
John Doe <triniteassih@gmail.com>
2024-08-01 15:20:08 +0000

    serializers auth

--- authentification/serializers.py
+++ authentification/serializers.py
@@ -4,12 +4,12 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth.hashers import make_password
 from .models import Client, Gerant
 
-class ClientSerializer(serializers.HyperlinkedModelSerializer):
+class ClientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Client
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
 
-class GerantSerializer(serializers.HyperlinkedModelSerializer):
+class GerantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Gerant
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
