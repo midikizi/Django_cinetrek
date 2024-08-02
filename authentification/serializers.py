d6dc88cded6bafccfa744303704c980b938da518
essodéké iréné MIDIKIZI <128699020+midikizi@users.noreply.github.com>
2024-08-02 16:01:28 +0100

    correction-sécurity

--- authentification/serializers.py
+++ authentification/serializers.py
@@ -9,7 +9,25 @@ class ClientSerializer(serializers.ModelSerializer):
         model = Client
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
 
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super(ClientSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            validated_data['password'] = make_password(validated_data['password'])
+        return super(ClientSerializer, self).update(instance, validated_data)
+
 class GerantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Gerant
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name']
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super(GerantSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        if 'password' in validated_data:
+            validated_data['password'] = make_password(validated_data['password'])
+        return super(GerantSerializer, self).update(instance, validated_data)
